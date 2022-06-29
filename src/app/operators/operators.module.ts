11abe3df6d5e78b9c296ebabb79373c081e96f48
ishumski <ishumski@scand.com>
2022-06-29 14:53:10 +0300

    init project

---
+++ src/app/operators/operators.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = []
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [],
+  exports: []
+})
+export class OperatorsModule {}
