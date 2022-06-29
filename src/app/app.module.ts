1560fd6fbf09fc5a2dbd20e9648b0ff101ec2b7c
ishumski <ishumski@scand.com>
2022-06-29 15:08:42 +0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { OperatorsModule } from './operators/operators.module';
 
 const routes: Routes = []
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), OperatorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/operators/operators.module.ts
+++ src/app/operators/operators.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { OperatorsComponent } from "./operators.component";
 
-const routes: Routes = []
+const routes: Routes = [{
+  path: 'operators',
+  component: OperatorsComponent
+}]
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [],
-  exports: []
+  declarations: [OperatorsComponent],
+  exports: [OperatorsComponent],
 })
 export class OperatorsModule {}
